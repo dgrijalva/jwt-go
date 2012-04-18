e49f2f5a18cd6ec5a9784db66a8010aa46df8e89
Dave Grijalva <grijalva@gmail.com>
2012-04-17 18:41:12 -0700

    almost working

--- rs256.go
+++ rs256.go
@@ -1,7 +1,16 @@
 package jwt
 
-type SigningMethodRS256 struct {
-}
+import (
+	"errors"
+	"encoding/base64"
+	"encoding/pem"
+	"crypto"
+	"crypto/x509"
+	"crypto/rsa"
+	_ "crypto/sha256"
+)
+
+type SigningMethodRS256 struct {}
 
 func init() {
 	RegisterSigningMethod("RS256", func() SigningMethod {
@@ -9,8 +18,36 @@ func init() {
 	})
 }
 
-func (m *SigningMethodRS256) Verify(signingString, signature string, key []byte)error {
-	return nil
+func (m *SigningMethodRS256) Verify(signingString, signature string, key []byte)(err error) {
+	// len % 4
+	switch len(signature) % 4 {
+		case 2:
+		signature = signature + "=="
+		case 3:
+		signature = signature + "==="
+	}
+	
+	// Key
+	var sig []byte
+	if sig, err = base64.URLEncoding.DecodeString(signature); err == nil {
+		var block *pem.Block
+		if block, _ = pem.Decode(key); block != nil {
+			var parsedKey interface{}
+			if parsedKey, err = x509.ParsePKIXPublicKey(block.Bytes); err == nil {
+				if rsaKey, ok := parsedKey.(*rsa.PublicKey); ok {
+					hasher := crypto.SHA256.New()
+					hasher.Write([]byte(signingString))
+					
+					err = rsa.VerifyPKCS1v15(rsaKey, crypto.SHA256, hasher.Sum(nil), sig)
+				} else {
+					err = errors.New("Key is not a valid RSA public key")
+				}
+			}
+		} else {
+			err = errors.New("Could not parse key data")
+		}
+	}
+	return
 }
 
 func (m *SigningMethodRS256) Sign(token, key []byte)error {
---
+++ rs256_test.go
@@ -0,0 +1,50 @@
+package jwt
+
+import (
+	"os"
+	"io"
+	"bytes"
+	"testing"
+	"strings"
+)
+
+var testData = []struct{
+	name string
+	tokenString string
+	claims map[string]interface{}
+	valid bool
+}{
+	{
+		"basic: foo => bar",
+		"eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJmb28iOiJiYXIifQ.FhkiHkoESI_cG3NPigFrxEk9Z60_oXrOT2vGm9Pn6RDgYNovYORQmmA0zs1AoAOf09ly2Nx2YAg6ABqAYga1AcMFkJljwxTT5fYphTuqpWdy4BELeSYJx5Ty2gmr8e7RonuUztrdD5WfPqLKMm1Ozp_T6zALpRmwTIW0QPnaBXaQD90FplAg46Iy1UlDKr-Eupy0i5SLch5Q-p2ZpaL_5fnTIUDlxC3pWhJTyx_71qDI-mAA_5lE_VdroOeflG56sSmDxopPEG3bFlSu1eowyBfxtu0_CuVd-M42RU75Zc4Gsj6uV77MBtbMrf4_7M_NUTSgoIF3fRqxrj0NzihIBg",
+		map[string]interface{}{"foo": "bar"},
+		true,
+	},
+	{
+		"basic invalid: foo => bar",
+		"eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJmb28iOiJiYXIifQ.EhkiHkoESI_cG3NPigFrxEk9Z60_oXrOT2vGm9Pn6RDgYNovYORQmmA0zs1AoAOf09ly2Nx2YAg6ABqAYga1AcMFkJljwxTT5fYphTuqpWdy4BELeSYJx5Ty2gmr8e7RonuUztrdD5WfPqLKMm1Ozp_T6zALpRmwTIW0QPnaBXaQD90FplAg46Iy1UlDKr-Eupy0i5SLch5Q-p2ZpaL_5fnTIUDlxC3pWhJTyx_71qDI-mAA_5lE_VdroOeflG56sSmDxopPEG3bFlSu1eowyBfxtu0_CuVd-M42RU75Zc4Gsj6uV77MBtbMrf4_7M_NUTSgoIF3fRqxrj0NzihIBg",
+		map[string]interface{}{"foo": "bar"},
+		false,
+	},
+}
+
+func TestRS256Verify(t *testing.T) {
+	file, _ := os.Open("test/sample_key.pub")
+	buf := new(bytes.Buffer)
+	io.Copy(buf, file)
+	key := buf.Bytes()
+	file.Close()
+	
+	for _, data := range testData {
+		parts := strings.Split(data.tokenString, ".")
+		
+		method, _ := GetSigningMethod("RS256")
+		err := method.Verify(strings.Join(parts[0:1], "."), parts[2], key)
+		if data.valid && err != nil {
+			t.Errorf("[%v] Error while verifying key: %v", data.name, err)
+		}
+		if !data.valid && err == nil {
+			t.Errorf("[%v] Invalid key passed validation", data.name)
+		}
+	}
+}
