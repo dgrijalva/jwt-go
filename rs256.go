498a013cdaeef7ab2ccf195b29f579adc9eb3496
rootlch <louish55@gmail.com>
2012-07-07 11:55:06 +0800

    Update master

--- rs256.go
+++ rs256.go
@@ -24,56 +24,70 @@ func (m *SigningMethodRS256) Alg() string {
 
 func (m *SigningMethodRS256) Verify(signingString, signature string, key []byte) (err error) {
 	// Key
-	var sig []byte
-	if sig, err = DecodeSegment(signature); err == nil {
-		var block *pem.Block
-		if block, _ = pem.Decode(key); block != nil {
-			var parsedKey interface{}
-			if parsedKey, err = x509.ParsePKIXPublicKey(block.Bytes); err == nil {
-				if rsaKey, ok := parsedKey.(*rsa.PublicKey); ok {
-					hasher := sha256.New()
-					hasher.Write([]byte(signingString))
+	sig, err := DecodeSegment(signature)
+	if err != nil {
+		return
 
-					err = rsa.VerifyPKCS1v15(rsaKey, crypto.SHA256, hasher.Sum(nil), sig)
-				} else {
-					err = errors.New("Key is not a valid RSA public key")
-				}
-			}
-		} else {
-			err = errors.New("Could not parse key data")
-		}
 	}
+
+	block, _ := pem.Decode(key)
+	if block == nil {
+		err = errors.New("Could not parse key data")
+		return
+	}
+
+	parsedKey, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		return
+	}
+
+	rsaKey, ok := parsedKey.(*rsa.PublicKey)
+	if !ok {
+		err = errors.New("Key is not a valid RSA public key")
+		return
+	}
+
+	hasher := sha256.New()
+	hasher.Write([]byte(signingString))
+	err = rsa.VerifyPKCS1v15(rsaKey, crypto.SHA256, hasher.Sum(nil), sig)
 	return
 }
 
 func (m *SigningMethodRS256) Sign(signingString string, key []byte) (sig string, err error) {
 	// Key
-	var rsaKey *rsa.PrivateKey
-	if rsaKey, err = m.parsePrivateKey(key); err == nil {
-		hasher := sha256.New()
-		hasher.Write([]byte(signingString))
-
-		var sigBytes []byte
-		if sigBytes, err = rsa.SignPKCS1v15(rand.Reader, rsaKey, crypto.SHA256, hasher.Sum(nil)); err == nil {
-			sig = EncodeSegment(sigBytes)
-		}
+	rsaKey, err := m.parsePrivateKey(key)
+	if err != nil {
+		return
 	}
-	return
+
+	hasher := sha256.New()
+	hasher.Write([]byte(signingString))
+
+	sigBytes, err := rsa.SignPKCS1v15(rand.Reader, rsaKey, crypto.SHA256, hasher.Sum(nil))
+	if err != nil {
+		return
+	}
+
+	sig = EncodeSegment(sigBytes)
 }
 
 func (m *SigningMethodRS256) parsePrivateKey(key []byte) (pkey *rsa.PrivateKey, err error) {
-	var block *pem.Block
-	if block, _ = pem.Decode(key); block != nil {
-		var parsedKey interface{}
-		if parsedKey, err = x509.ParsePKCS1PrivateKey(block.Bytes); err != nil {
-			if parsedKey, err = x509.ParsePKCS8PrivateKey(block.Bytes); err != nil {
-				return nil, err
-			}
-		}
-		var ok bool
-		if pkey, ok = parsedKey.(*rsa.PrivateKey); !ok {
-			err = errors.New("Key is not a valid RSA private key")
-		}
+	block, _ := pem.Decode(key)
+	if block == nil {
+		return
 	}
-	return
+
+	parsedKey, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	parsedKey, err = x509.ParsePKCS8PrivateKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	if pkey, ok := parsedKey.(*rsa.PrivateKey); !ok {
+		err = errors.New("Key is not a valid RSA private key")
+	}
 }
