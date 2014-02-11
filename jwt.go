8de1bb8a61e3762ad358d32aa995ff5ec87b4ec9
Dave Grijalva <grijalva@gmail.com>
2014-02-10 17:27:59 -0800

    updated TimeFunc documentation

--- jwt.go
+++ jwt.go
@@ -9,8 +9,9 @@ import (
 	"time"
 )
 
-// TimeFunc is used when parsing token to validate "exp" claim (expiration time).
-// You can override it to use another time zone (UTC for example).
+// TimeFunc provides the current time when parsing token to validate "exp" claim (expiration time).
+// You can override it to use another time value.  This is useful for testing or if your
+// server uses a different time zone than your tokens.
 var TimeFunc = time.Now
 
 // Parse methods use this callback function to supply
