2c907dbb70d7a49388fa978ef681d77f8f01d2a5
Dave Grijalva <grijalva@gmail.com>
2014-06-15 19:32:57 -0700

    comments

--- doc.go
+++ doc.go
@@ -1,4 +1,4 @@
-// A Go implementation of JSON Web Tokens: http://self-issued.info/docs/draft-jones-json-web-token.html
+// Package jwt is a Go implementation of JSON Web Tokens: http://self-issued.info/docs/draft-jones-json-web-token.html
 // 
 // See README.md for more info.
 package jwt
