ff92e16c5fbf675f57c228f2ad34c8eead5b31dd
Eric Lagergren <ericscottlagergren@gmail.com>
2015-09-15 18:19:24 -0700

    signing_method.go (re: https://github.com/dgrijalva/jwt-go/issues/75#issuecomment-140589538)
    Make the RegisterSigningMethod, GetSigningMethod, and the new
    RemoveSigningMethod function all thread-safe.

---
+++ signing_method_test.go
@@ -0,0 +1,46 @@
+package jwt
+
+import (
+	"testing"
+
+	jwt "github.com/SermoDigital/jwt-go"
+)
+
+type MySigningMethod struct{}
+
+func (m *MySigningMethod) Verify(signingString, signature string, key interface{}) error {
+	return nil
+}
+func (m *MySigningMethod) Sign(signingString string, key interface{}) (string, error) {
+	return "", nil
+}
+func (m *MySigningMethod) Alg() string { return "MySigningMethod's Cool Algorithm" }
+
+func TestRegisterSigningMethod(t *testing.T) {
+
+	jwt.RegisterSigningMethod("SuperSignerAlgorithm1000", func() jwt.SigningMethod {
+		return &MySigningMethod{}
+	})
+
+	if jwt.GetSigningMethod("SuperSignerAlgorithm1000") == nil {
+		t.Error("Expected SuperSignerAlgorithm1000, got nil")
+	}
+
+	jwt.RemoveSigningMethod("SuperSignerAlgorithm1000")
+}
+
+func TestRemoveSigningMethod(t *testing.T) {
+	jwt.RegisterSigningMethod("SuperSignerAlgorithm1000", func() jwt.SigningMethod {
+		return &MySigningMethod{}
+	})
+
+	if jwt.GetSigningMethod("SuperSignerAlgorithm1000") == nil {
+		t.Error("Expected SuperSignerAlgorithm1000, got nil")
+	}
+
+	jwt.RemoveSigningMethod("SuperSignerAlgorithm1000")
+
+	if a := jwt.GetSigningMethod("SuperSignerAlgorithm1000"); a != nil {
+		t.Errorf("Expected nil, got %v", a)
+	}
+}
