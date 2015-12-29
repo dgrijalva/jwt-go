f7288992d27b8f629462eb45c8b9dd3e9face204
Martin Lindhe <martin@ubique.se>
2015-12-29 22:51:54 +0100

    split ErrInvalidKey into ErrInvalidType and ErrInvalidKey

--- errors.go
+++ errors.go
@@ -6,7 +6,8 @@ import (
 
 // Error constants
 var (
-	ErrInvalidKey       = errors.New("key is invalid or of invalid type")
+	ErrInvalidKey       = errors.New("key is invalid")
+	ErrInvalidType      = errors.New("key is of invalid type")
 	ErrHashUnavailable  = errors.New("the requested hash function is unavailable")
 	ErrNoTokenInRequest = errors.New("no token present in request")
 )
