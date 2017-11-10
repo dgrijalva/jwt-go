c92f3677b3889fff19f86681e6d70efce75e3732
benniekrijger <benniekrijger@hotmail.com>
2017-11-10 12:14:13 +0100

    Move test to jwt namespace

--- token_test.go
+++ token_test.go
@@ -1,13 +1,12 @@
-package jwt_test
+package jwt
 
 import (
 	"testing"
-	"github.com/dgrijalva/jwt-go"
 )
 
 func TestDecodeSegment(t *testing.T) {
 	validSegment := "nxEWX7UD76fRupczm4MWL0B4UQh/tGpjSdWi6z/wlkw="
-	_, err := jwt.DecodeSegment(validSegment)
+	_, err := DecodeSegment(validSegment)
 	if err != nil {
 		t.Errorf("Unable to decode segment %s", validSegment)
 	}
