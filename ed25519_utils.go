13b8828f893ae6bb3594fe1e296ff1ff5afa6047
BluBb-mADe <886641+BluBb-mADe@users.noreply.github.com>
2018-07-25 09:31:57 +0200

    added proper asn1 parsing

--- ed25519_utils.go
+++ ed25519_utils.go
@@ -1,20 +1,38 @@
 package jwt
 
 import (
+	"encoding/asn1"
 	"encoding/pem"
 	"golang.org/x/crypto/ed25519"
 )
 
+type OI struct {
+	ObjectIdentifier asn1.ObjectIdentifier
+}
 
-// Pseudo decoder hack, someone should probably implement proper asn1 parsing for this stuff
+type ed25519PrivKey struct {
+	Version          int
+	ObjectIdentifier OI
+	PrivateKey       []byte
+}
+
+type ed25519PubKey struct {
+	OBjectIdentifier OI
+	PublicKey        asn1.BitString
+}
+
 // https://tools.ietf.org/id/draft-ietf-curdle-pkix-06.html#rfc.section.7
 func ParseED25519PrivateKeyFromPEM(key []byte) (*ed25519.PrivateKey, error) {
 	var block *pem.Block
 	if block, _ = pem.Decode(key); block == nil {
 		return nil, ErrKeyMustBePEMEncoded
 	}
-
-	privKey := ed25519.NewKeyFromSeed(block.Bytes[16:])
+	var asn1PrivKey ed25519PrivKey
+	if _, err := asn1.Unmarshal(block.Bytes, &asn1PrivKey); err != nil {
+		return nil, err
+	}
+	// we don't need the tag and length bytes
+	privKey := ed25519.NewKeyFromSeed(asn1PrivKey.PrivateKey[2:])
 	return &privKey, nil
 }
 
@@ -23,7 +41,10 @@ func ParseED25519PublicKeyFromPEM(key []byte) (*ed25519.PublicKey, error) {
 	if block, _ = pem.Decode(key); block == nil {
 		return nil, ErrKeyMustBePEMEncoded
 	}
-
-	pkey := ed25519.PublicKey(block.Bytes[12:])
+	var asn1PubKey ed25519PubKey
+	if _, err := asn1.Unmarshal(block.Bytes, &asn1PubKey); err != nil {
+		return nil, err
+	}
+	pkey := ed25519.PublicKey(asn1PubKey.PublicKey.Bytes)
 	return &pkey, nil
 }
