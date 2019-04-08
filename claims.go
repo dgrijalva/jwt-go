879bc07fc9376a1b1b9950767419f24250683227
Dave Grijalva <grijalva@gmail.com>
2019-04-08 13:17:27 -0700

    s/Id/ID/

--- claims.go
+++ claims.go
@@ -21,7 +21,7 @@ type Claims interface {
 type StandardClaims struct {
 	Audience  string `json:"aud,omitempty"`
 	ExpiresAt int64  `json:"exp,omitempty"`
-	Id        string `json:"jti,omitempty"`
+	ID        string `json:"jti,omitempty"`
 	IssuedAt  int64  `json:"iat,omitempty"`
 	Issuer    string `json:"iss,omitempty"`
 	NotBefore int64  `json:"nbf,omitempty"`
