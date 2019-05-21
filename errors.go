e74cbe9f2eb54e71e129db4a306dcbdf82d8f7a4
Dave Grijalva <grijalva@gmail.com>
2019-05-21 14:30:14 -0700

    WIP on incorporating support for Go2 errors (and backward compatible xerrors package)

--- errors.go
+++ errors.go
@@ -2,15 +2,50 @@ package jwt
 
 import (
 	"errors"
+	"fmt"
 	"time"
 )
+
+// Copied from xerrors, for compatibility without requiring the xerrors package
+type errorPrinter interface {
+	Print(args ...interface{})
+	Printf(format string, args ...interface{})
+	Detail() bool
+}
 
 // Error constants
 var (
 	ErrInvalidKey      = errors.New("key is invalid")
-	ErrInvalidKeyType  = errors.New("key is of invalid type")
+	ErrInvalidKeyType  = NewInvalidKeyTypeError("", "")
 	ErrHashUnavailable = errors.New("the requested hash function is unavailable")
 )
+
+type InvalidKeyTypeError struct {
+	expected, received string
+}
+
+func (e *InvalidKeyTypeError) Error() string {
+	if e.expected == "" && e.received == "" {
+		return "key is of invalid type"
+	}
+	return fmt.Sprintf("key is of invalid type: expected %v, received %v", e.Unwrap(), e.expected, e.received)
+}
+
+func (e *InvalidKeyTypeError) Format(f fmt.State, c rune) {
+	if c == '+' {
+		f.Write([]byte(e.Error()))
+	} else {
+		f.Write([]byte(ErrInvalidKeyType.Error()))
+	}
+}
+
+func (e *InvalidKeyTypeError) Unwrap() error {
+	return ErrInvalidKeyType
+}
+
+func NewInvalidKeyTypeError(expected, received string) error {
+	return &InvalidKeyTypeError{expected, received}
+}
 
 // The errors that might occur when parsing and validating a token
 const (
@@ -21,7 +56,6 @@ const (
 	// Standard Claim validation errors
 	ValidationErrorAudience      // AUD validation failed
 	ValidationErrorExpired       // EXP validation failed
-	ValidationErrorIssuedAt      // IAT validation failed
 	ValidationErrorIssuer        // ISS validation failed
 	ValidationErrorNotValidYet   // NBF validation failed
 	ValidationErrorID            // JTI validation failed
@@ -38,20 +72,25 @@ func NewValidationError(errorText string, errorFlags uint32) *ValidationError {
 
 // ValidationError is the error from Parse if token is not valid
 type ValidationError struct {
-	Inner  error  // stores the error returned by external dependencies, i.e.: KeyFunc
+	inner  error  // stores the error returned by external dependencies, i.e.: KeyFunc
 	Errors uint32 // bitfield.  see ValidationError... constants
 	text   string // errors that do not have a valid error just have text
 }
 
 // Validation error is an error type
 func (e ValidationError) Error() string {
-	if e.Inner != nil {
+	if e.inner != nil {
 		return e.Inner.Error()
 	} else if e.text != "" {
 		return e.text
 	} else {
 		return "token is invalid"
 	}
+}
+
+// Unwrap implements xerrors.Wrapper
+func (e ValidationError) Unwrap() error {
+	return e.inner
 }
 
 // No errors
@@ -62,7 +101,7 @@ func (e *ValidationError) valid() bool {
 // ExpiredError allows the caller to know the delta between now and the expired time and the unvalidated claims.
 // A client system may have a bug that doesn't refresh a token in time, or there may be clock skew so this information can help you understand.
 type ExpiredError struct {
-	Now       int64
+	Now       time.Time
 	ExpiredBy time.Duration
 	Claims
 }
@@ -70,3 +109,5 @@ type ExpiredError struct {
 func (e *ExpiredError) Error() string {
 	return "Token is expired"
 }
+
+type MultiError []error
