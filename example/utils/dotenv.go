2f15f2ad114030616d7bbf375ce732d81201c057
Restu Wahyu Saputra <restuwahyu13@gmail.com>
2021-04-25 06:37:07 +0700

    Update dotenv.go

--- example/utils/dotenv.go
+++ example/utils/dotenv.go
@@ -7,12 +7,13 @@ import (
 )
 
 func GodotEnv(key string) string {
-	os.Setenv("GO_ENV", "development")
 	env := make(chan string, 1)
 
 	if os.Getenv("GO_ENV") != "production" {
 		godotenv.Load(".env")
 		env <- os.Getenv(key)
+	} else {
+		 env <- os.Getenv(key)
 	}
 
 	return <-env
